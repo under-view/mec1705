309531c9b14a106f5d5e5acaf941fcd5d08624bb
Vincent Davis Jr <vince@underview.tech>
2023-03-01 14:01:50 -0600

    compile-load-alias: fix DEVUTILS_DIR bug
    Problem:
    	* Absolute path to files directory
    	  is incorrect.
    
    Fixes:
    	* Use realpath with dirname is acquire
    	  correct absolute path.
    
    Additions:
    	* script is an executable
    	* Changes the variable name DEVUTILS_DIR
    	  to ILI9488_DEVUTILS_DIR. That way if
    	  a similar script with similar variable
    	  is sourced there's less of a chance
    	  of being overridden.
    	* adds mount_enable_acpi_configfs alias
    	  for easier loading of acpi-configfs
    	  kernel module.
    
    Signed-off-by: Vincent Davis Jr <vince@underview.tech>

--- devutils/compile-load-alias.sh
+++ devutils/compile-load-alias.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-DEVUTILS_DIR=$(dirname "${BASH_SOURCE[0]}")
+MEC1705_DEVUTILS_DIR=$(dirname $(realpath "${BASH_SOURCE[0]}"))
 
-alias compile_mec1705_aml="make -C ${DEVUTILS_DIR}/../ aml"
-alias load_mec1705_aml="mkdir -p /sys/kernel/config/acpi/table/mec1705; cat ${DEVUTILS_DIR}/../mec1705.aml > /sys/kernel/config/acpi/table/mec1705/aml"
-alias unload_mec1705_aml="${DEVUTILS_DIR}/unload-acpi-table /sys/kernel/config/acpi/table/mec1705"
+alias mount_enable_acpi_configfs="mount -t configfs none /sys/kernel/config 2>/dev/null && modprobe acpi-configfs"
+alias compile_mec1705_aml="make -C ${MEC1705_DEVUTILS_DIR}/../ aml"
+alias load_mec1705_aml="mkdir -p /sys/kernel/config/acpi/table/mec1705; cat ${MEC1705_DEVUTILS_DIR}/../mec1705.aml > /sys/kernel/config/acpi/table/mec1705/aml"
+alias unload_mec1705_aml="${MEC1705_DEVUTILS_DIR}/unload-acpi-table /sys/kernel/config/acpi/table/mec1705"
